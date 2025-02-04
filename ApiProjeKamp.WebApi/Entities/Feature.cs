cca8a3595fe4484a9f5b4f81e5e230f36a992b2f
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-04 13:11:42 +0300

    Entityler eklendi

---
+++ ApiProjeKamp.WebApi/Entities/Feature.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProjeKamp.WebApi.Entities
+{
+    public class Feature
+    {
+        public int FeatureID { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Description { get; set; }
+        public string VideoURL { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
---
+++ ApiProjeKamp.WebApi/Entities/Reservation.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiProjeKamp.WebApi.Entities
+{
+    public class Reservation
+    {
+        public int ReservationID { get; set; }
+        public string NameSurname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateOnly ReservationDate { get; set; }
+        public string ReservationTime { get; set; }
+        public int CountOfPeople { get; set; }
+        public string Message { get; set; }
+        public string ReservationStatus { get; set; }
+    }
+}
