667e7c0f14e0dcd609198f3922f2c979a4b955cb
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-07 12:36:29 +0300

    Context eklendi

---
+++ ApiProjeKamp.WebApi/Context/ApiContext.cs
@@ -0,0 +1,25 @@
+﻿using ApiProjeKamp.WebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProjeKamp.WebApi.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-EOIN7IK\\SQLEXPRESS;database=ApiYummyDb;Integrated Security = True;TrustServerCertificate=true;");
+            //Değiştir
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Chef> Chefs { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
+
