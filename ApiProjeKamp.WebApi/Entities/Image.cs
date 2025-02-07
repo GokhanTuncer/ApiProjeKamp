3d0fc8c40e959c368fefff4dca7b9f1064858db2
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-07 12:16:34 +0300

    Contact,Image,Message enttityleri eklendi

---
+++ ApiProjeKamp.WebApi/Entities/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProjeKamp.WebApi.Entities
+{
+    public class Image
+    {
+        public int ImageID { get; set; }
+        public string Title { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
