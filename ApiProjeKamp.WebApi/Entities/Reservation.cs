7b8219deb61e1deded2f604d9f6f2a9bc97d9113
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-07 12:40:09 +0300

    entitty fix ve migration

--- ApiProjeKamp.WebApi/Entities/Reservation.cs
+++ ApiProjeKamp.WebApi/Entities/Reservation.cs
@@ -6,7 +6,7 @@
         public string NameSurname { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public DateOnly ReservationDate { get; set; }
+        public DateTime ReservationDate { get; set; }
         public string ReservationTime { get; set; }
         public int CountOfPeople { get; set; }
         public string Message { get; set; }
