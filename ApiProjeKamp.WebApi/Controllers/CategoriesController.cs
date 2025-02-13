358dbcc6ec33be86f0b58198c2125cd7b2252d25
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-13 11:48:17 +0300

    Kategori controlleri eklendi

---
+++ ApiProjeKamp.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using ApiProjeKamp.WebApi.Context;
+using ApiProjeKamp.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKamp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Kategori Ekleme işlemi başarılı");
+        }
+    }
+}
