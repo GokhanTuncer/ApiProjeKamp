2d0c1c76dcd3fd327d346283a2c35937528ab5fb
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-03-28 15:39:32 +0300

    Contact için DTOlar ve controller eklendi

---
+++ ApiProjeKamp.WebApi/Controllers/ContactsController.cs
@@ -0,0 +1,41 @@
+﻿using ApiProjeKamp.WebApi.Context;
+using ApiProjeKamp.WebApi.DTOs;
+using ApiProjeKamp.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKamp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ContactsController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult ContactList()
+        {
+            var values = _context.Contacts.ToList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateContact(CreateContactDTO createContactDTO)
+        {
+            var contact = new Contact
+            {
+                MapLocation = createContactDTO.MapLocation,
+                Address = createContactDTO.Address,
+                Phone = createContactDTO.Phone,
+                Email = createContactDTO.Email,
+                OpenHours = createContactDTO.OpenHours
+            };
+            _context.Contacts.Add(contact);
+            _context.SaveChanges();
+            return Ok("İletişim bilgisi ekleme işlemi başarılı");
+        }
+    }
+}
