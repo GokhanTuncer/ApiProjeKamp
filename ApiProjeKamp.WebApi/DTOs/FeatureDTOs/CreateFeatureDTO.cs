ce8a16b0f9d683e92e870cd7c7c344fb0000dcf6
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-04-02 13:54:09 +0300

    Message Dtoları eklendi

--- ApiProjeKamp.WebApi/DTOs/FeatureDTOs/CreateFeatureDTO.cs
+++ ApiProjeKamp.WebApi/DTOs/FeatureDTOs/CreateFeatureDTO.cs
@@ -2,7 +2,7 @@
 {
     public class CreateFeatureDTO
     {
-        public int FeatureID { get; set; }
+       
         public string Title { get; set; }
         public string SubTitle { get; set; }
         public string Description { get; set; }
