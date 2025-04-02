80f7b4c25fda3f781884ac8f9d5be3b5c1f90daf
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-04-02 14:09:04 +0300

    Mapping sınıfı eklendi

---
+++ ApiProjeKamp.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,23 @@
+﻿using ApiProjeKamp.WebApi.DTOs.FeatureDTOs;
+using ApiProjeKamp.WebApi.DTOs.MessageDTOs;
+using ApiProjeKamp.WebApi.Entities;
+using AutoMapper;
+
+namespace ApiProjeKamp.WebApi.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping() 
+        {
+            CreateMap<Feature, ResultFeatureDTO>().ReverseMap();  
+            CreateMap<Feature, UpdateFeatureDTO>().ReverseMap();  
+            CreateMap<Feature, CreateFeatureDTO>().ReverseMap();  
+            CreateMap<Feature, GetByIDFeatureDTO>().ReverseMap();
+
+            CreateMap<Message, ResultMessageDTO>().ReverseMap();
+            CreateMap<Message, UpdateMessageDTO>().ReverseMap();
+            CreateMap<Message, CreateMessageDTO>().ReverseMap();
+            CreateMap<Message, GetByIDMessageDTO>().ReverseMap();
+        }
+    }
+}
