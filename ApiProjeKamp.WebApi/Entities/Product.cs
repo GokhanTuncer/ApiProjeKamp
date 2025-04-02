934ea36763d8c96d7ce53a9ae334c56717fd39b3
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-04-02 15:53:12 +0300

    Kategori ürün ilşkisi yapıldı + migration

--- ApiProjeKamp.WebApi/Entities/Product.cs
+++ ApiProjeKamp.WebApi/Entities/Product.cs
@@ -7,5 +7,7 @@
         public string ProductDescription { get; set; }
         public decimal Price { get; set; }
         public string ImageURL { get; set; }
+        public int? CategoryID { get; set; }
+        public Category Category { get; set; }
     }
 }
