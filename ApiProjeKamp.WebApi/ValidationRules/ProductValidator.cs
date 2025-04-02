bfea6608e872d784696f8b44fd080a6cbf7417df
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-04-02 15:09:21 +0300

    Fluent Validator paketi eklendi ve Ürün sınıfı için validatorlar eklendi.

---
+++ ApiProjeKamp.WebApi/ValidationRules/ProductValidator.cs
@@ -0,0 +1,23 @@
+﻿using ApiProjeKamp.WebApi.Entities;
+using FluentValidation;
+
+namespace ApiProjeKamp.WebApi.ValidationRules
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator() 
+        {
+            RuleFor(x => x.ProductName).NotEmpty().WithMessage("Ürün adı boş geçilemez");
+            RuleFor(x => x.ProductName).MinimumLength(2).WithMessage("Ürün adı en az 2 karakter olmalıdır");
+            RuleFor(x => x.ProductName).MaximumLength(50).WithMessage("Ürün adı en fazla 50 karakter olmalıdır");
+
+            RuleFor(x =>x.Price).NotEmpty().WithMessage("Ürün fiyatı boş geçilemez");
+            RuleFor(x => x.Price).LessThan(0).WithMessage("Ürün fiyatı negatif olamaz");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Ürün fiyatı 0'dan büyük olmalıdır");
+
+            RuleFor(x => x.ProductDescription).NotEmpty().WithMessage("Ürün açıklaması boş geçilemez");
+
+        }
+
+    }
+}
