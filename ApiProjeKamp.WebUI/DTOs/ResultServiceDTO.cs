7ecdbbc1c0481d4a6c23f9083058db9e6312f7af
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-09 14:37:16 +0300

    Service'in API Consume edilmesi

---
+++ ApiProjeKamp.WebUI/DTOs/ResultServiceDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiProjeKamp.WebUI.DTOs
+{
+    public class ResultServiceDTO
+    {
+        public int ServiceID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string IconURL { get; set; }
+    }
+}
--- ApiProjeKamp.WebUI/ViewComponents/_ServiceDefaultViewComponent.cs
+++ ApiProjeKamp.WebUI/ViewComponents/_ServiceDefaultViewComponent.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiProjeKamp.WebUI.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ApiProjeKamp.WebUI.ViewComponents
 {
     public class _ServiceDefaultViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServiceDefaultViewComponent(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7246/api/Services/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDTO>>(jsonData);
+            }
+            else
+            {
+               
+            }
             return View();
         }
     }
