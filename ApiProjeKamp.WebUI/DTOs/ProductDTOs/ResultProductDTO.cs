0c166cf763c275d39af003cbda298b33b9f8204b
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-12 13:28:03 +0300

    Menü ürünlerinin listelenmesi

---
+++ ApiProjeKamp.WebUI/DTOs/ProductDTOs/ResultProductDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProjeKamp.WebUI.DTOs.ProductDTOs
+{
+    public class ResultProductDTO
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageURL { get; set; }
+        public int CategoryID { get; set; }
+    }
+}
