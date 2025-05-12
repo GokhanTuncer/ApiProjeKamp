2247830f126f7ce21c20a45852e3a78f8170f331
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-12 14:13:13 +0300

    Referans bölümü vc'ye alınması

---
+++ ApiProjeKamp.WebApi/Controllers/TestimonialsController.cs
@@ -0,0 +1,56 @@
+﻿using ApiProjeKamp.WebApi.Context;
+using ApiProjeKamp.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKamp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public TestimonialsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult TestimonialList()
+        {
+            var values = _context.Testimonials.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTestimonial(Testimonial testimonial)
+        {
+            _context.Testimonials.Add(testimonial);
+            _context.SaveChanges();
+            return Ok("Referans Ekleme işlemi başarılı");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var testimonial = _context.Testimonials.Find(id);
+            _context.Testimonials.Remove(testimonial);
+            _context.SaveChanges();
+            return Ok("Referans Silme işlemi başarılı");
+        }
+        [HttpGet("GetTestimonial")]
+        public IActionResult GetTestimonial(int id)
+        {
+            var value = _context.Testimonials.Find(id);
+            return Ok(value);
+        }
+        [HttpPut]
+        public IActionResult UpdateTestimonial(Testimonial testimonial)
+        {
+            _context.Testimonials.Update(testimonial);
+            _context.SaveChanges();
+            return Ok("Referans Güncelleme işlemi başarılı");
+        }
+    }
+}
