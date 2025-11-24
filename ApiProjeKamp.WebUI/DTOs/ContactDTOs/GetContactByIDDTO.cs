36355479d2da6b8b1e96aacfd7d54b8b5fe2354d
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-11-24 16:31:31 +0300

    İletişim bölümü UI

---
+++ ApiProjeKamp.WebUI/DTOs/ContactDTOs/GetContactByIDDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProjeKamp.WebUI.DTOs.ContactDTOs
+{
+    public class GetContactByIDDTO
+    {
+        public int ContactID { get; set; }
+        public string MapLocation { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string OpenHours { get; set; }
+    }
+}
