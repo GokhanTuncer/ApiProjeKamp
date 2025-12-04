e16a965df29f51a91c5cc64c8288bc6baf280238
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-12-04 14:56:39 +0300

    Rezervasyon main chart api ekleme

--- ApiProjeKamp.WebUI/ViewComponents/DashboardViewComponent/_DashboardMainChartComponentPartial.cs
+++ ApiProjeKamp.WebUI/ViewComponents/DashboardViewComponent/_DashboardMainChartComponentPartial.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiProjeKamp.WebUI.DTOs.ReservationDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ApiProjeKamp.WebUI.ViewComponents.DashboardViewComponent
 {
     public class _DashboardMainChartComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardMainChartComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri("https://localhost:7246/");
+
+            var response = await client.GetAsync("api/Reservations/GetReservationStats");
+            var json = await response.Content.ReadAsStringAsync();
+
+            var data = JsonConvert.DeserializeObject<List<ReservationChartDTO>>(json);
+
+            return View(data);
         }
     }
 }
