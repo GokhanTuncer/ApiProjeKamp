e7c247276fda890236b966f8fe280e3cbbaf7edd
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-12-16 13:41:42 +0300

    Anasayfada api üzerinden istatistiklerin getirilmesi

---
+++ ApiProjeKamp.WebApi/Controllers/StatisticsController.cs
@@ -0,0 +1,44 @@
+﻿using ApiProjeKamp.WebApi.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKamp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public StatisticsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("ReservationCount")]
+        public IActionResult ReservationCount()
+        {
+            var reservationCount = _context.Reservations.Count();
+            return Ok(reservationCount);
+        }
+        [HttpGet("ProductCount")]
+        public IActionResult ProductCount()
+        {
+            var productCount = _context.Products.Count();
+            return Ok(productCount);
+        }
+        [HttpGet("ChefCount")]
+        public IActionResult ChefCount()
+        {
+            var chefCount = _context.Chefs.Count();
+            return Ok(chefCount);
+        }
+        [HttpGet("TotalGuestCount")]
+        public IActionResult TotalGuestCount()
+        {
+            var totalGuestCount = _context.Reservations.Sum(r => r.CountOfPeople);
+            return Ok(totalGuestCount);
+        }
+
+    }
+}
---
+++ ApiProjeKamp.WebUI/ViewComponents/HomePageViewComponents/_HomePageStatisticsComponentPartial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKamp.WebUI.ViewComponents.HomePageViewComponents
+{
+    public class _HomePageStatisticsComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _HomePageStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client1 = _httpClientFactory.CreateClient();
+            var response = await client1.GetAsync("https://localhost:7246/api/Statistics/ProductCount");
+            var jsonData1 = await response.Content.ReadAsStringAsync();
+            ViewBag.v1 = jsonData1;
+
+            var client2 = _httpClientFactory.CreateClient();
+            var response2 = await client2.GetAsync("https://localhost:7246/api/Statistics/ReservationCount");
+            var jsonData2 = await response2.Content.ReadAsStringAsync();
+            ViewBag.v2 = jsonData2;
+
+            var client3 = _httpClientFactory.CreateClient();
+            var response3 = await client1.GetAsync("https://localhost:7246/api/Statistics/ChefCount");
+            var jsonData3 = await response3.Content.ReadAsStringAsync();
+            ViewBag.v3 = jsonData3;
+
+            var client4 = _httpClientFactory.CreateClient();
+            var response4 = await client4.GetAsync("https://localhost:7246/api/Statistics/TotalGuestCount");
+            var jsonData4 = await response4.Content.ReadAsStringAsync();
+            ViewBag.v4 = jsonData1;
+
+            return View();
+        }
+    }
+
+}
