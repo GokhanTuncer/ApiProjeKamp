6b653ace3a2bf2869d53996c3d1870ffb552b250
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-12-17 17:39:48 +0300

    anasayfa rezervasyon düzenlenmesi

--- ApiProjeKamp.WebUI/Controllers/DefaultController.cs
+++ ApiProjeKamp.WebUI/Controllers/DefaultController.cs
@@ -1,11 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiProjeKamp.WebUI.DTOs.ContactDTOs;
+using ApiProjeKamp.WebUI.DTOs.ReservationDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
 
 namespace ApiProjeKamp.WebUI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateReservationDTO createReservationDTO)
+        {
+            createReservationDTO.ReservationStatus = "Onay Bekliyor";
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createReservationDTO);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7246/api/Reservations", stringContent);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
